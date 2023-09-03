0eb6d22883239898cbf60cfb3bd817205802bd6e
Muhammad Jazlan <ja.zl.an@live.com>
2023-09-03 21:13:56 +0500

    Created a simple client master store with hashing (dictionary

--- python/client.py
+++ python/client.py
@@ -5,7 +5,7 @@ from sys import argv
 if len(argv) < 3:
     exit(Exception("Usage: python[3] client.py <host> <port>"))
 
-host, port = argv[1], int(argv[2])
+host, port = argv[1], argv[2]
 
 
 class Client:
@@ -18,7 +18,7 @@ class Client:
             port (int): contains the port number of the server
         """
         self.host = host
-        self.port = port
+        self.port = int(port)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             self.socket.connect((self.host, self.port))
@@ -29,33 +29,69 @@ class Client:
         print(f"Connected to server at {self.host}:{self.port}")
 
     def put(self, KV):
+        """
+        Summary:
+            Sends a key-value pair to the server
+        Args:
+            KV (tuple<key, value>): contains the key-value pair to be sent to the server
+
+        Returns:
+            Bool: returns True if the key-value pair was sent successfully, else returns False
+        """
         if KV:
-            val = KV.key + ":" + KV.value
+            val = "PUT " + KV.key + ":" + KV.value
             self.socket.send(val.encode())
             return True
         else:
             return False
 
-    async def get(self, key):
+    def get(self, key):
+        """
+        Summary:
+            Gets the value of a key from the server
+        Args:
+            key (string): contains the key to be searched
+
+        Returns:
+            string: returns the value of the key if found, else returns "MISS". If no key is provided, returns "Please enter a key"
+        """
         if key:
-            self.socket.send(key.encode())
-            return await self.socket.recv(1024)
+            message = "GET " + key
+            self.socket.send(message.encode())
+            res = self.socket.recv(1024).decode()
+            return res
+        else:
+            return "Please enter a key"
 
     def delete(self, key):
-        pass
+        """
+        Summary:
+            Deletes a key-value pair from the server
+        Args:
+            key (string): contains the key to be deleted
+        """
+        if key:
+            message = "DELETE " + key
+            self.socket.send(message.encode())
+            response = self.socket.recv(1024).decode()
+            if response == "DELETED":
+                return f"Key {key} deleted successfully"
+            else:
+                return f"Key {key} not found at server"
+        else:
+            return "Please enter a key"
 
-    def main(self):
+    def run(self):
         """
         Summary:
             Main function for the client. It takes user input for commands and communicates with the server, carrying out appropriate actions
 
         Legal Inputs:
-            1. PUT : <key> <value>
-            2. GET : <key>
-            3. DELETE : <key>
-            4. EXIT
+            - PUT : <key> <value>
+            - GET : <key>
+            - DELETE : <key>
+            - EXIT
         """
-
         try:
             while True:
                 prompt = input("datastore: ")
@@ -67,26 +103,25 @@ class Client:
                     key = prompt[1]
                     self.put(kv)
                 elif prompt[0] == "GET":
+                    key = prompt[1]
                     print(self.get(key))
-
                 elif prompt[0] == "DELETE":
                     key = prompt[1]
-                    self.delete(key)
+                    print(self.delete(key))
                 elif prompt[0] == "EXIT":
                     break
                 else:
                     print("Invalid command")
         except KeyboardInterrupt:
             print("\nExiting...")
-            self.socket.close()  # close the socket
+            self.socket.close()  # close the socket connection on keyboard interrupt
 
 
 def main():
     client = Client(host, port)
 
-    # client.main()
+    client.run()
 
 
 if __name__ == "__main__":
-    # main()
-    print(Client(host, port).__init__.__doc__)
+    main()
--- python/master.py
+++ python/master.py
@@ -1,42 +1,91 @@
-import os
 from time import time
 import socket
+from sys import argv
+from threading import Thread
+
+if len(argv) < 3:
+    exit(Exception("Usage: python[3] master.py <host> <port>"))
+
+host, port = argv[1], argv[2]
 
 
 class KV:
     def __init__(self, key, value):
         self.key = key
         self.value = value
-        self.time = time()
+        # self.time = time() # removing time for now, since this would be part of a protocol later
 
 
 class Master:
     def __init__(self, host, port):
-        self.storePath = os.getcwd() + "/store/temp.txt"
+        self.store = dict()  # key: key, value:
         self.host = host
-        self.port = port
+        self.port = int(port)
+        self.threads = list()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((self.host, self.port))
 
-    def store(self, KV):
-        if KV:
-            with open(self.storePath, "a") as f:
-                f.write(KV.key + ":" + KV.value + "\n")
-                f.close()
-                return True
+    def put(self, kv: KV):
+        if kv:
+            self.store[kv.key] = kv.value
+
+    def get(self, key):
+        return self.store.get(key, "MISS")
+
+    def delete(self, key):
+        if key in self.store:
+            del self.store[key]
+            return True
+        else:
+            return False
+
+    def clientHandler(self, client: socket.socket):
+        while True:
+            cmd = client.recv(1024).decode()
+            cmd = cmd.split(" ")
+            if cmd[0] == "PUT":
+                key, value = cmd[1].split(":")
+                self.put(KV(key, value))
+                print(f"PUT {key}:{value}")
+                self.currentState()
+            elif cmd[0] == "GET":
+                val = self.get(cmd[1])
+                client.send(val.encode())
+            elif cmd[0] == "DELETE":
+                val = self.delete(cmd[1])
+                if val:
+                    client.send("DELETED".encode())
+                else:
+                    client.send("MISS".encode())
+            else:
+                client.send("Invalid command. Disconnecting.".encode())
+                client.close()
+                break
+
+    def run(self):
+        print(f"Server listening on {self.host}:{self.port}")
+        self.socket.listen(25)  # arbitrary number
+        try:
+            while True:
+                client, address = self.socket.accept()
+                print(f"Connected to {address}")
+                t = Thread(target=self.clientHandler, args=(client,))
+                self.threads.append(t)
+                t.start()
+        except KeyboardInterrupt:
+            print("Shutting down server...")
+            for t in self.threads:
+                t.join()
+            self.socket.close()
+            print("Server shut down successfully.")
+
+    def currentState(self):
+        print(self.store)
 
 
 def main():
-    master = Master("localhost", 8081)
-
-    while True:
-        master.socket.listen(5)
-        conn, addr = master.socket.accept()
-        data = conn.recv(1024)
-        if data:
-            data = data.decode()
-            kv = KV(data.split(":")[0], data.split(":")[1])
-            master.store(kv)
+    master = Master(host, port)
+    master.run()
 
 
 if __name__ == "__main__":
